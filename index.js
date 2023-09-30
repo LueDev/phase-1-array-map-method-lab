5c34e8c5147809c72550a7e0dcd026ff9695bca9
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-09-30 13:05:27 -0400

    Passing all tests.

--- index.js
+++ index.js
@@ -12,5 +12,19 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newTitleCasedTutorials = []
+  let titleCasing = tutorials.map((element) => {
+      let titledCasedElement = [] //holds the element which will be titlecased
+      let splitty = element.split(" ") // split the element into words. 
+      splitty.map((word) => { //for each word, split into letters
+        let splitWord = word.split("") //splitting into letters
+        splitWord.splice(0, 1, splitWord[0].toUpperCase()) //destructively replace first element to uppercase, including symbols.
+        let assembledWord = splitWord.join("") //join the letters to reform the titlecased word
+        titledCasedElement.push(assembledWord) //the temp array housing each word upon each iteration
+      })
+      newTitleCasedTutorials.push(titledCasedElement.join(" ")) //the array containing all elements titlecased
+  })
+  return newTitleCasedTutorials 
 }
+
+console.log(titleCased())
